b502a43bff207e84ab29495bc1d8836ac8e17f32
honghe <hehong0102@hotmail.com>
2020-10-23 23:18:17 +0200

    created login logout test case

--- frontend-tests/cypress/integration/regression-test-suite.sprc.js
+++ frontend-tests/cypress/integration/regression-test-suite.sprc.js
@@ -4,6 +4,15 @@ describe('regression test suite', function(){
   
     it('perform valid login', function(){
         cy.visit('http://localhost:3000')
+        cy.title().should('include','Hotel')
+        cy.contains('Login')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.user > .btn').click()
+         cy.contains('Login')
+
       
     })
       
