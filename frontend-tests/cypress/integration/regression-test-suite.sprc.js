3c132c2ab37943c6bd282397678d45a83cbdf7f1
honghe <hehong0102@hotmail.com>
2020-10-23 14:52:19 +0200

    created .gitignore and test project

---
+++ frontend-tests/cypress/integration/regression-test-suite.sprc.js
@@ -0,0 +1,10 @@
+//<reference types="cypress" />
+
+describe('regression test suite', function(){
+  
+    it('perform valid login', function(){
+        cy.visit('http://localhost:3000')
+      
+    })
+      
+})
